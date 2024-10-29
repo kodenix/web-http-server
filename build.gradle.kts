65127baa6a143a5e3157e17be3c07d022d77e1f5
Alberto Morales <kodenix@gmail.com>
2024-10-29 20:44:47 +0100

    chore: add assertjs and rest-assured

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,6 +14,10 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     implementation(kotlin("stdlib-jdk8"))
+    // https://mvnrepository.com/artifact/org.assertj/assertj-core
+    testImplementation("org.assertj:assertj-core:3.6.1")
+    // https://mvnrepository.com/artifact/io.rest-assured/rest-assured
+    testImplementation("io.rest-assured:rest-assured:3.0.0")
 }
 
 tasks.test {
